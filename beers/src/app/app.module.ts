b83579bb00ebfb2e64da6a659b1cff42cd5f729f
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-08-22 17:47:18 +0100

    created form component

--- beers/src/app/app.module.ts
+++ beers/src/app/app.module.ts
@@ -2,22 +2,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { CardsTemplateComponent } from './components/cards-template/cards-template.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { ProductComponent } from './components/pages/product/product.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +41,7 @@ import { ContactComponent } from './components/pages/contact/contact.component';
     ProductComponent,
     AboutComponent,
     ContactComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,6 +53,12 @@ import { ContactComponent } from './components/pages/contact/contact.component';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
   ],
   providers: [],
   bootstrap: [AppComponent],
