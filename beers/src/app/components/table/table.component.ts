464b515450e8e49aa0d9ec89c9c9be0be6b4aa24
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-08-31 18:19:12 +0100

    remove favorite from local storage

--- beers/src/app/components/table/table.component.ts
+++ beers/src/app/components/table/table.component.ts
@@ -8,11 +8,10 @@ import { Beer } from 'src/app/Beers';
 })
 export class TableComponent implements OnInit {
   @Input()
-   //array with all bears
+  //array with all bears
   beers!: Beer[];
 
-
-  displayedColumns: string[] = ['position', 'name'];
+  displayedColumns: string[] = ['position', 'name', 'btn'];
 
   dataSource: any = [];
 
@@ -20,7 +19,32 @@ export class TableComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.beers);
-    //we receive beer as a prop in favorite and do a map 
-    this.dataSource = this.beers.map((beer, index) =>({ position: index+1, name: beer.name}))
+    //we receive beer as a prop in favorite and do a map to show on the screen
+    this.dataSource = this.beers.map((beer, index) => ({
+      position: index + 1,
+      name: beer.name,
+      id: beer.id,
+    }));
+  }
+
+  removeFavorite(id: number) {
+    console.log('removeFavorite');
+    const data: Beer[] = JSON.parse(localStorage.getItem('favorite') || '[]');
+
+    //filter the data from local storage with the same name of the beer clicked and return a new array with the ids different from the id clicked
+    const removeBeer = data.filter((beer: any) => beer.id !== id);
+
+    //save the data again in local storage without the beer removed
+    localStorage.setItem('favorite', JSON.stringify(removeBeer));
+
+    // this.dataSource.name = removeBeer;
+
+this.dataSource = removeBeer.map(
+      (beer, index: number) => ({
+        position: index + 1,
+        name: beer.name,
+        id: beer.id,
+      })
+    );
   }
 }
